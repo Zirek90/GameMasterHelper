72b77dae534ce472cee8aef62da544c7fc4077fe
Mateusz Grzymowicz <mateusz.grzymowicz@gmail.com>
2018-11-16 20:37:28 +0100

    added post request to form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { FormListComponent } from './form-list/form-list.component';
@@ -14,7 +16,9 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-form',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  name:string;
+  hp:number;
+  category:string;
+  resp:any;
+
+  constructor(private http: HttpClient) {
+   }
 
   ngOnInit() {
+  }
+
+  submit() {
+ 
+    this.http.post('https://monsterlist-59e3a.firebaseio.com/monsters.json', {
+      name: this.name,
+      hp: this.hp,
+      category: this.category
+    })
+    .subscribe((resp) => {
+      this.resp = resp;
+      console.log(this.resp);
+      this.name = '';
+      this.hp = null;
+      this.category = '';
+    })
   }
 
 }
