23351be37a2b96049744b0968f6e9a36eab43c65
Mateusz Grzymowicz <mateusz.grzymowicz@gmail.com>
2018-11-16 19:27:22 +0100

    added stoper to header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,16 +8,30 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   dateInfo: string;
+  counter:number = 0;
+  stoper:number;
+  interval:any;
 
   constructor() { 
     setInterval(() => {
       let currentDate = new Date();
       this.dateInfo = currentDate.toLocaleTimeString();
     },1000)
-
   }
 
   ngOnInit() {
+  }
+
+  stoperStart() {
+    if(this.counter != 0) { this.counter = 0};
+     
+    this.interval = setInterval(() => {
+      this.counter++;
+      this.stoper = this.counter;
+    },1000)
+  }
+  stoperStop() {
+    clearInterval(this.interval)
   }
 
 }
